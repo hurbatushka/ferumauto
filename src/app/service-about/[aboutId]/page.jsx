97e7eaf0234fca786b428da1988e3ff1ea43878f
Иван Хурбатов <hurbatushka@gmail.com>
2023-10-30 14:36:51 +0300

    забыл элиас

--- src/app/service-about/[aboutId]/page.jsx
+++ src/app/service-about/[aboutId]/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../../page.module.css';
+import styles from '@/app/page.module.css';
 
 export default function AboutService({ params: { aboutId } }) {
   return <main className={styles.main}>О сервисе №{aboutId}</main>;
